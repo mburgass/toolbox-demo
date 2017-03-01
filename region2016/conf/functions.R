2230dbcee90b63740b8ab8434be4e83bfec66559
mburgass <m.burgass14@imperial.ac.uk>
2017-03-01 13:46:50 -0800

    run calculate scores with new data and models

--- region2016/conf/functions.R
+++ region2016/conf/functions.R
@@ -351,8 +351,11 @@ AO = function(layers,
   d2 <- SelectLayersData(layers, layers = 'ao_need', narrow=TRUE) %>%
     select(region_id = id_num, year, need = val_num)
 
+  d3 <- SelectLayersData(layers, layers = 'ao_poverty', narrow=TRUE) %>%
+    select(region_id = id_num, year, poverty = val_num)
   ## join data layers into single data frame (see RStudio cheatsheets)
-  ao_data <- left_join(d1, d2, by="region_id")
+  ao_data <- left_join(d1, d2, by="region_id")%>%
+    left_join(d3, by="region_id")
 
   ############ MODEL ##############
   ## this step calculates status scores of all years, using the goal model.
@@ -362,8 +365,8 @@ AO = function(layers,
   ## Note that "Sustainability" and "status_year" have been defined at the start of the AO function
 
   ao_model <- ao_data %>%
-    mutate(Du = (1 - need) * (1 - access)) %>%
-    mutate(status = (1 - Du/2) * sustainability)
+    mutate(Du = (1 - (need + poverty)/2) * (1 - access)) %>%
+    mutate(status = (1 - Du) * sustainability)
   # head(ao_model); summary(ao_model)
 
   ############ STATUS ##############
